5c416f8fb519c8eceaa938aa9e7691125f340b61
Daniel Pires <danielpires6@hotmail.com>
2020-12-19 21:54:52 -0300

    feat: Adicionado POC de Get usuários

---
+++ Backend/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Usuario
+    {
+        public Usuario(string nome, string cidade)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Cidade = cidade;
+            DataInsercao = new DateTime();
+
+        }
+        public Guid Id { get; }
+        public string Nome { get; set; }
+        public string Cidade { get; set;}
+        public DateTime? DataInsercao { get; set; }
+    }
+}
---
+++ Backend/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models;
+
+namespace Backend.Repositories
+{
+    interface IUsuarioRepository
+    {
+        void Adicionar(Usuario usuario);
+        void Alterar(Usuario usuario);
+        IEnumerable<Usuario> ListarUsuarios();
+        Usuario Obterusuario(Guid id);
+        void RemoverUsuario(Guid id);
+    }
+}
