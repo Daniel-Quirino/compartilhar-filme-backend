1d7b6ae6e10b7688f42d3099fde11f19252e653b
lucasdipaula <lucasdepaula@dcc.ufmg.br>
2021-01-20 14:11:34 -0300

    testes de unidade usuario

--- Tests/UserTest.cs
+++ Tests/UserTest.cs
@@ -1,20 +1,31 @@
+
 using System;
 using Xunit;
 using Backend.Models;
-
 namespace Test.User
-{
+{​
     public class UserTest
-    {
+    {​
         [Fact]
         public void CriarUsuario()
-        {
+        {​
             Usuario novoUsuario = new Usuario("Lucas", "Belo Horizonte");
-
             Assert.Equal("Lucas", novoUsuario.Nome);
             Assert.Equal("Belo Horizonte", novoUsuario.Cidade);
-
-        }
-
-    }
-}
+        }​
+        [Fact]
+        public void CriarUsuarioDadosDiferentes()
+        {​
+            Usuario novoUsuario = new Usuario("Daniel", "Diamantina");
+            Assert.Equal("Daniel", novoUsuario.Nome);
+            Assert.Equal("Diamantina", novoUsuario.Cidade);
+        }​
+        [Fact]
+        public void FormatarNomeComLetrasMaiusculaIniciais()
+        {​
+            Usuario novoUsuario = new Usuario("DANIEL QUIRINO", "Diamantina");
+            Assert.Equal("Daniel Quirino", Usuario.formatarNomeComLetrasMaiusculaIniciais("DANIEL QUIRINO"));
+            Assert.Equal("Diamantina", novoUsuario.Cidade);
+        }​
+    }​
+}​
