9612979321376e5ec345299bdab6df56ed36f5de
Daniel Quirino <danielpires6@hotmail.com>
2021-02-07 08:52:18 -0300

    test: fazer deploy novamente

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
 
 mongoose.Promise = global.Promise
 
+
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
