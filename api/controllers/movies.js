7ad91d6bf6e7c0b6809fcfa80fba07238365b5f6
Daniel Quirino <danielpires6@hotmail.com>
2021-02-27 17:34:56 -0300

    feature: adicionado atributo descricao do filme

--- api/controllers/movies.js
+++ api/controllers/movies.js
@@ -45,6 +45,7 @@ exports.createMovie = (req, res, next) => {
                 price: result.image,
                 rate: result.rate,
                 _id: result._id,
+                description: result.description,
             }
         })
     })
@@ -57,7 +58,7 @@ exports.createMovie = (req, res, next) => {
 exports.getMovie = (req, res, next) => {
     const id = req.params.movieId;
     Movie.findById(id)
-        .select("title image views likes notes rate")
+        .select("title image views likes notes rate description")
         .exec()
         .then(doc => {
             doc ? res.status(200).json({movie:doc}) : res.status(404).json({message: 'No valid entry found for provider id'})
--- api/models/movie.js
+++ api/models/movie.js
@@ -7,7 +7,8 @@ const movieSchema = mongoose.Schema({
     views: { type: Number, default: 0 },
     likes: { type: Boolean, default: false },
     notes: { type: Number, default: 0 },
-    rate: { type: Number, default: 0 }
+    rate: { type: Number, default: 0 },
+    description:{ type: String, default: '' },
 });
 
 module.exports = mongoose.model('Movie', movieSchema)
